4a1ec4cb376c76de2f3ae52816415c9724c05a84
Patrick Miller <paddymills@proton.me>
2023-04-15 14:33:23 -0400

    impl env

--- src/proj5/src/Env.java
+++ src/proj5/src/Env.java
@@ -4,21 +4,27 @@ import java.util.HashMap;
 public class Env
 {
     public Env prev;
+    private HashMap<String, Object> entries;
     public Env(Env prev)
     {
+        this.prev = prev;
     }
     public void Put(String name, Object value)
     {
+        this.entries.put(name, value);
     }
     public Object Get(String name)
     {
+        if ( this.entries.containsKey(name) )
+            return this.entries.get(name);
+
         // this is a fake implementation
         // For the real implementation, I recommend to return a class object
         //   since the identifier's type can be variable or function
         //   whose detailed attributes will be different
-        if(name.equals("a") == true) return "int";
-        if(name.equals("b") == true) return "bool";
-        if(name.equals("testfunc") == true) return "func()->int";
+        // if(name.equals("a") == true) return "int";
+        // if(name.equals("b") == true) return "bool";
+        // if(name.equals("testfunc") == true) return "func()->int";
         return null;
     }
 }
