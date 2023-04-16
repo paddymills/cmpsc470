6ae3110c5aa0de6fa1f7969876a0f5c8155ce1b7
Patrick Miller <paddymills@proton.me>
2023-04-16 01:54:45 +0000

    env passed tests

--- src/proj5/src/Env.java
+++ src/proj5/src/Env.java
@@ -4,25 +4,36 @@ import java.util.HashMap;
 public class Env
 {
     public Env prev;
-    public Env(Env prev)
-    {
+    private HashMap<String, Object> table;
+    
+    public Env(Env prev) {
+        this.prev = prev;
     }
-    public void Put(String name, Object value)
-    {
+    
+    public void Put(String name, Object value) {
+        // first call: initialize table HashMap
+        if ( this.table == null )
+            this.table = new HashMap<String, Object>();
+
+        this.table.put(name, value);
     }
 
-    public Object Get(String name)
-    {
-        if ( this.entries.containsKey(name) )
-            return this.entries.get(name);
+    public Object Get(String name) {
+        if ( this.table != null && this.table.containsKey(name) )
+            return this.table.get(name);
+
+        if ( this.prev != null )
+            return this.prev.Get(name);
+
+        return null;
 
         // this is a fake implementation
         // For the real implementation, I recommend to return a class object
         //   since the identifier's type can be variable or function
         //   whose detailed attributes will be different
-        if(name.equals("a") == true) return "int";
-        if(name.equals("b") == true) return "bool";
-        if(name.equals("testfunc") == true) return "func()->int";
-        return null;
+        // if(name.equals("a") == true) return "int";
+        // if(name.equals("b") == true) return "bool";
+        // if(name.equals("testfunc") == true) return "func()->int";
+        // return null;
     }
 }
