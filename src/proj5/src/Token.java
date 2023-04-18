04eab61fe092cd1879083fe03305aec6b55d5261
Patrick Miller <paddymills@proton.me>
2023-04-18 20:03:05 +0000

    add parseint

--- src/proj5/src/Token.java
+++ src/proj5/src/Token.java
@@ -1,8 +1,13 @@
 public class Token
 {
     public String lexeme;
-    public Token(String lexeme)
-    {
+
+    public Token(String lexeme) {
         this.lexeme = lexeme;
+    }
+
+    public int parseInt() throws Exception {
+        return Integer.parseInt(lexeme);
+
     }
 }
